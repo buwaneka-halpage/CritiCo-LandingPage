478bca2106f756b5251db439b25086511ceb6079
hhh-berzerk <hhhbhuwaneka@gmail.com>
2026-03-18 18:09:36 +0530

    fix: set base to '/' for Vercel root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/critico-full/',
+  base: '/',
   plugins: [
     preact(),
     tailwindcss(),
